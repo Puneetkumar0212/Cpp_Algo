5af7ae704d8881b10a93dd653bdd2b7fe6c7f91f
Ankita Khurana <ankita0096@gmail.com>
2017-09-08 22:36:36 +0530

    questions

---
+++ Posist Technologies/ques3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+
+
+	int n ;
+	cin>>n ;
+	string s[n];
+	for(int i = 0 ; i<n ;i++ )
+		cin>>s[i];
+
+	if(n%2==0)
+		cout<<s[n-1];
+	else 
+	{
+		
+	}
+}
