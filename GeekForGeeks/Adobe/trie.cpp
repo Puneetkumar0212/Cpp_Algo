39d62d25a7de125d94b3bab4b792b078ab641ac7
Ankita Khurana <ankita0096@gmail.com>
2018-01-14 11:25:34 +0530

    trie

---
+++ GeekForGeeks/Adobe/trie.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+struct TrieNode
+{
+	struct TrieNode *child[26];
+	bool is_last;
+};
+
+TrieNode* getNode()
+{
+	TrieNode *r = new TrieNode;
+	r->is_last=false;
+	for ( int i = 0 ; i < 26 ; i ++)
+		r->child[i]=NULL;
+	return r;
+
+}
+
+void insert (TrieNode *root , string key )
+{
+
+}
+int main()
+{
+	string input_chars[] = {"there","the"};
+	int size = sizeof(input_chars)/sizeof(input_chars[0]);
+	TrieNode *root = getNode();
+
+	for (int i = 0; i < n; i++)
+        insert(root, input_chars[i]);
+
+
+	return 0 ;
+}
