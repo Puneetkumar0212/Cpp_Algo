41d7028c4cb351b2e3b48500052dddbfc1e7d61e
Ankita Khurana <ankita0096@gmail.com>
2018-01-14 11:49:06 +0530

    trie display

--- GeekForGeeks/Adobe/trie.cpp
+++ GeekForGeeks/Adobe/trie.cpp
@@ -35,15 +35,50 @@ void insert (TrieNode *root , string key )
 temp->is_last=true;
 
 }
+
+bool IsLeafNode (TrieNode *node)
+{
+	return node->is_last!=false;		//is last node -> returns true
+}
+
+void display(TrieNode *root,char str[], int level)
+{
+	//Add null incase last node (EOS)
+	if(IsLeafNode(root))
+	{
+		str[level]='\0';
+		cout<<str<<"\n";
+	}
+	
+	for (int i = 0 ; i < 26 ; i ++)
+	{
+		if(root->child[i])		//if child exists 
+		{
+			str[level]= i+'a';
+			display(root->child[i],str,level+1);//Recursively call display till EOS
+
+		}
+	}
+}
+
+
 int main()
 {
 	string input_chars[] = {"there","the"};
 	int size = sizeof(input_chars)/sizeof(input_chars[0]);
 	TrieNode *root = getNode();
 
+	//Insert All words 	
 	for (int i = 0; i < size; i++)
         insert(root, input_chars[i]);
+    
 
+    //To Display Trie formed
+    int level = 0 ;
+    char str[20];	//Max 20 words
+
+
+    display(root, str,level);	
 
 	return 0 ;
 }
