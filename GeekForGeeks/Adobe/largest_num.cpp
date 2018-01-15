dcafef512d038577775a1146f55b872d23aa7528
Ankita Khurana <ankita0096@gmail.com>
2018-01-15 17:14:25 +0530

    max num

---
+++ GeekForGeeks/Adobe/largest_num.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+	int arr[] = {1,2,3,4,5,5,3,6};
+	int digits[]={0,0,0,0,0,0,0,0,0,0};
+
+	int size = sizeof(arr)/sizeof(arr[0]);
+	for(int i = 0 ; i < size ; i ++)
+	{
+		digits[arr[i]]++;
+	}
+	for(int i = 9 ; i >=0; i --)
+	{
+		while(digits[i]--)
+		{
+			cout<<i;
+		}
+	}
+
+
+
+}
